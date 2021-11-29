080fa263186e8d5d52d22957541111ddf5e8bc2f
Klemen Ledinek <klemen.ledinek@gmail.com>
2021-11-29 22:29:57 +0100

    File for dooing examples

---
+++ src/example.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int i = 1;
+    std::cout << "Insert a numeber: ";
+    std::cin >> i;
+    std::cout << "You entered " << i << '\n';
+    return 0;        
+}
